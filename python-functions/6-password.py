719940bff5f2dd4f6842654c11c05cfb77b4bd64
caminspired <charlotteaowusuansah@gmail.com>
2023-11-30 10:10:12 +0000

    task 6 project 2

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -1,21 +1,20 @@
 def validate_password(password):
     validate = True
     if len(password) < 8:
-        validate = False
+        return not validate
     if not any(char.isupper() for char in password):
-        validate = False
+        return not validate
     if not any(char.islower() for char in password):
-        validate = False
+        return not validate
     if not any(char.isdigit() for char in password):
-        validate = False
+        return not validate
     if any(char.isspace() for char in password):
-        validate = False
+        return not validate
 
     if not validate:
         return False
     else:
         return True
-    
     
 print(validate_password("Password123"))
 print(validate_password("abc123"))
