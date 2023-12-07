dc09f8b13125b4f67d000555d02c97a3a68fb3e3
caminspired <charlotteaowusuansah@gmail.com>
2023-12-07 13:32:21 +0000

    1 args

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,17 @@
+import sys 
+
+if __name__ == "__main__":
+    argv = sys.argv[1:]
+    num_arguments = len(argv)
+    
+    if num_arguments == 0:
+        print("{} arguments: \n".format(num_arguments))
+        
+    elif num_argumentss == 1:
+        print("{} argument: \n".format(num_arguments))
+        
+    else:
+        print("{} arguments: \n".format(num_argumentss))
+        
+        for i, arg in enumerate(argv, start=1):
+            print("{}: {}".format(i, argument))
