5750c4fbe1afe50b86af646026b1780b4f97f423
caminspired <charlotteaowusuansah@gmail.com>
2023-12-07 15:45:40 +0000

    raise exception

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -2,17 +2,24 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except ZeroDivisionError:
-        print("Inside result: None")
+        print("Inside result: {}".format(None))
         return None
     except Exception as e:
-        print("Inside result: None")
+        print("Inside result: {}".format(None))
         return None
     else:
         print("Inside result: {}".format(result))
         return result
     finally:
         if 'result' in locals():
-            if b != 0:
-                print("{} / {} = {}".format(a, b, result))
+            print("{} / {} = {}".format(a, b, result) if b != 0 else "{} / {} = {}".format(a, b, None))
         else:
-            print("{} / {} = None".format(a, b))
+            print("{} / {} = {}".format(a, b, None))
+
+# Example usage:
+result1 = safe_print_division(10, 2)
+result2 = safe_print_division(0, 2)
+a = 10
+b = -2
+result = safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, result))
