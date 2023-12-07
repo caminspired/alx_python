1cbd2c6c5f6cb44bebf0e131d4f942645811c875
caminspired <charlotteaowusuansah@gmail.com>
2023-12-07 15:06:47 +0000

    3 div py

---
+++ python-import_modules/3-safe_print_division.py
@@ -0,0 +1,17 @@
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        print("Inside result: {}".format(None))
+        return None
+    except Exception as e:
+        print("Inside result: {}".format(None))
+        return None
+    else:
+        print("Inside result: {}".format(result))
+        return result
+    finally:
+        if 'result' in locals():
+            print("{} / {} = {}".format(a, b, result)) 
+        else:
+            print("{} / {} = {}".format(a, b, None))
