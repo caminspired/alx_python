d829fa1988258467362a7f94c1a6acbacd87424a
caminspired <charlotteaowusuansah@gmail.com>
2023-12-07 14:16:53 +0000

    1arg py

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -4,15 +4,15 @@ if __name__ == "__main__":
     argv = sys.argv[1:]
     num_arg = len(argv)
     
-    for i, arg in enumerate(argv):
-        if num_arg == 0:
-            print("{} arguments:".format(num_arg))
-            
-        elif num_arg == 1:
-            print("{} argument:".format(num_arg))
+for i, arg in enumerate(argv, start=1):
+    if num_arg == 0:
+        print("{} arguments:".format(num_arg))
+        
+    elif num_arg == 1:
+        print("{} argument:".format(num_arg))
+        print("{}: {}".format(i, arg))
+        
+    else:
+        print("{} arguments:".format(num_arg))
+        for i, arg in enumerate(argv, start=1):
             print("{}: {}".format(i, arg))
-            
-        else:
-            print("{} arguments:".format(num_arg))
-            for i, arg in enumerate(argv, start=1):
-                print("{}: {}".format(i, arg))
