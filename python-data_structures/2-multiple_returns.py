03333c433bc7188dbda14bf7efd5196eac633e23
caminspired <charlotteaowusuansah@gmail.com>
2023-12-07 16:46:34 +0000

    update p2 t2

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -2,5 +2,5 @@ def multiple_returns(sentence):
     if len(sentence) == 0:
         return (0, None)
     else:
+        return (len(sentence), sentence[0])
         print ("Length: {} - First character: {}".format(len(sentence), (sentence[0])))
-    
