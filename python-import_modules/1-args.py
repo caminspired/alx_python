ccdc76c3e3f7cbab6d272e1ce271fa64b80083e6
caminspired <charlotteaowusuansah@gmail.com>
2023-12-07 13:40:18 +0000

    chnages 1 arg

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -9,12 +9,11 @@ if __name__ == "__main__":
         
     elif num_arguments == 1:
         print("{} argument: \n".format(num_arguments))
-        
-        for i, arg in enumerate(argv, start=1):
+        for i, argument in enumerate(argv, start=1):
             print("{}: {}".format(i, argument))
         
     else:
         print("{} arguments: \n".format(num_arguments))
         
-        for i, arg in enumerate(argv, start=1):
+        for i, argument in enumerate(argv, start=1):
             print("{}: {}".format(i, argument))
