81d5258ec652eab77b090244f1615c1c08e7cd3f
caminspired <charlotteaowusuansah@gmail.com>
2023-12-07 12:45:25 +0000

    execute

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,9 @@
-a = 1
-b = 2
+if __name__ == "__main__":
+    a = 1
+    b = 2
 
-from add_0 import add
+    from add_0 import add
 
-add(a = a, b = b)
+    add(a = a, b = b)
 
-print("{a} + {b} = {sum}".format(a = a, b = b, sum = add(a, b)))
+    print("{a} + {b} = {sum}".format(a = a, b = b, sum = add(a, b)))
