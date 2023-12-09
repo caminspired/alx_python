e7a41c0ff3857c4c8328eae1118c74bf53557c6d
caminspired <charlotteaowusuansah@gmail.com>
2023-12-09 16:55:58 +0000

    task 2 update

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -16,16 +16,18 @@ class Square:
         __size (int): Private instance attribute representing the size of the square.
     """
 
-    def __init__(self, size):
+    def __init__(self, size=0):
         """Initializes a new instance of the Square class.
 
         Args:
             size (int): The size of the square.
-
+            
         Raises:
             TypeError: If size is not an integer.
             ValueError: If size is less than 0.
         """
+        
+        type(size) == int
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -33,7 +35,8 @@ class Square:
 
         self.__size = size
 
-    def get_area(self):
+
+    def area(self):
         """Calculates and returns the area of the square.
 
         Returns:
