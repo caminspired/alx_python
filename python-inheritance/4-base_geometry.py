409cb52f903ae9581743325627da135e236430d4
caminspired <charlotteaowusuansah@gmail.com>
2023-12-09 21:05:33 +0000

    task 4

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,15 @@
+"""
+This module
+"""
+class BaseGeometry:
+    """A base class for geometry operations."""
+
+    def area(self):
+        """
+        Calculate the area.
+
+        Raises:
+            Exception: This method is not implemented in the base class.
+        """
+        raise Exception("area() is not implemented")
+    
