1a59f37501d1a1474a5a27b7d9cd28c474def68f
caminspired <charlotteaowusuansah@gmail.com>
2023-12-09 21:30:30 +0000

    up

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -6,9 +6,6 @@ class BaseGeometry:
     """
     An empty base class for geometry operations.
 
-    Methods:
-        __init_subclass__(cls, **kwargs): This method is called when a subclass is created.
-
     Attributes:
         None
     """
