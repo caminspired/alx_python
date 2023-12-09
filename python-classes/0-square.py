8e7bb86c04a4abfd920c91d78b951e57c3e49eea
caminspired <charlotteaowusuansah@gmail.com>
2023-12-09 15:59:36 +0000

    classes p1 t0 update

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,14 @@
 class Square():
-        
+    
+     """
+     This class defines a Square. 
+     
+     It will have a private instance attribute = size.
+     """ 
+       
     def __init__(self, size):
         self.__size = size
+        
+    """
+    A new instance of a square is initialized with private attribute = size.
+    """
