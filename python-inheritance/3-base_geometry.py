a6d543e19ef752ae71564be353c48ff98c732dd9
caminspired <charlotteaowusuansah@gmail.com>
2023-12-11 20:56:38 +0000

    3 up

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,9 +1,5 @@
-"""
-This module contains an empty class.
-"""
+""" This module contains an empty class"""
 
 class BaseGeometry:
-    """
-    An empty base class for geometry operations.
-    """
+    """An empty base class for geometry operations"""
     pass
