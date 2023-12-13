01a50db1fb4bf16f4f36e9535fdf8aa3a862a462
caminspired <charlotteaowusuansah@gmail.com>
2023-12-13 20:48:49 +0000

    up

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -5,8 +5,3 @@
 class BaseGeometry:
     """ defines the class BaseGeometry """
     pass
-
-
-def dir(cls):
-    """ removes __init__subclass from dir() """
-    return [attribute for  attribute in super().dir() if attribute != "init_subclass"] 
