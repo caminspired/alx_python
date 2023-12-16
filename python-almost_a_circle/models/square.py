427912d17598bad97f9462b6f836cc1a988904b3
caminspired <charlotteaowusuansah@gmail.com>
2023-12-16 22:22:12 +0000

    task 10 circle

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -157,3 +157,22 @@ class Square(Rectangle):
         """ string representation of the square """
         
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+    
+    @property
+    def size(self):
+        """ getter method for size """
+        
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ setter method for size """
+        
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """ string representation of the square """
+        
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width)
