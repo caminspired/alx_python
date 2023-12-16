2ab5d14b8325e4d7c996fffa2cc3360a09fb6038
caminspired <charlotteaowusuansah@gmail.com>
2023-12-16 21:30:23 +0000

    update

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -22,62 +22,62 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
                 
-    class Rectangle(Base):
-        """ 
-        defines class Rectangle as a subclass of class Base
+class Rectangle(Base(id)):
+    """ 
+    defines class Rectangle as a subclass of class Base
+    
+    Attributes:
+    __width -> width
+    __height -> height
+    __x -> x
+    __y -> y
+    """
+    
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ initializes an instance of class Rectangle """
         
-        Attributes:
-        __width -> width
-        __height -> height
-        __x -> x
-        __y -> y
-        """
-        
-        def __init__(self, width, height, x=0, y=0, id=None):
-            """ initializes an instance of class Rectangle """
-            
-            super().__init__(id)
-            self.width = width
-            self.height = height
-            self.x = x
-            self.y = y
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
-        @property
-        def width(self):
-            """ getter method for width """
-            return self.__width
+    @property
+    def width(self):
+        """ getter method for width """
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            """ setter method for width """
-            self.__width = value
+    @width.setter
+    def width(self, value):
+        """ setter method for width """
+        self.__width = value
 
-        @property
-        def height(self):
-            """ getter method for height """
-            return self.__height
+    @property
+    def height(self):
+        """ getter method for height """
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            """ setter method for height """
-            self.__height = value
+    @height.setter
+    def height(self, value):
+        """ setter method for height """
+        self.__height = value
 
-        @property
-        def x(self):
-            """ getter method for x """
-            return self.__x
+    @property
+    def x(self):
+        """ getter method for x """
+        return self.__x
 
-        @x.setter
-        def x(self, value):
-            """ setter method for x """
-            self.__x = value
+    @x.setter
+    def x(self, value):
+        """ setter method for x """
+        self.__x = value
 
-        @property
-        def y(self):
-            """ getter method for y """
-            return self.__y
+    @property
+    def y(self):
+        """ getter method for y """
+        return self.__y
 
-        @y.setter
-        def y(self, value):
-            """ setter method for y """
-            self.__y = value
+    @y.setter
+    def y(self, value):
+        """ setter method for y """
+        self.__y = value
