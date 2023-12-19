7803ee04dde62ec2412413e36bc364f3318caff2
caminspired <charlotteaowusuansah@gmail.com>
2023-12-19 20:06:11 +0000

    update

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,26 +1,7 @@
 """ This module contains the class Base """
 
-class Base:
-    """ defines class Base with private attribute __nb_objects """
-    __nb_objects = 0
-    
-    
-    def __init__(self, id=None):
-        """
-        initializes an instance of class Base 
-        
-        Variables"
-        __nb_objects = 0
-        
-        Args:
-        id = None
-        """
-        
-        if id is not None:
-            self.id = id
-        else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+from model.rectangle import Rectangle
+""" imports class Rectangle into the module """
                 
 class Rectangle(Base):
     """ 
