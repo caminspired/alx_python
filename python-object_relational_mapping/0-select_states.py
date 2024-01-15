58d875e566781834485e090de39f4d98c20f31f6
caminspired <charlotteaowusuansah@gmail.com>
2024-01-15 22:11:45 +0000

    0 orm

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,25 +1,30 @@
 # this module contains an SQL script that lists all cities from a database
 import MySQLdb
 
-#assigns variables and values
-db = MySQLdb.connect(host="localhost",
-                      user="username",
-                      passwd="password",
-                      db="hbtn_0e_0_usa",
-                      port="3306")
- 
-cursor = db.cursor()
+#define functions 
+def list_states(username, password, database):
 
-#executes SQL command in Python
-cursor.execute("SELECT * FROM states ORDER BY states.id")
+    #assigns variables and values
+    db = MySQLdb.connect(host="localhost",
+                        user="username",
+                        passwd="password",
+                        db="hbtn_0e_0_usa",
+                        port=3306
+                        )
+    
+    cursor = db.cursor()
 
-states = cursor.fetchall()
+    #executes SQL command in Python
+    cursor.execute("SELECT * FROM states ORDER BY states.id")
+
+    states = cursor.fetchall()
+
+    #print results of query 
+    for state in states:
+        print(state)
 
 #commit commands to execute
 db.commit()
-
-#print results of query 
-print(states)
 
 #close all connections
 cursor.close()
