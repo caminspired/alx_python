c2c800924ef68d946da818a0b7c6ca67a914597c
caminspired <charlotteaowusuansah@gmail.com>
2024-01-16 20:41:05 +0000

    0 orm py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,44 +1,34 @@
-# this module contains an SQL script that lists all cities from a database
 import MySQLdb
-import sys
 
-#define functions 
-def list_states(username, password, database):
+try:
+    # Connect to the MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        user="username",
+        passwd="password",
+        db="hbtn_0e_0_usa",
+        port=3306
+    )
 
-    #assigns variables and values
-    db = MySQLdb.connect(host="localhost",
-                        user="username",
-                        passwd="password",
-                        db="hbtn_0e_0_usa",
-                        port=3306
-                        )
-    
+    # Create a cursor object to interact with the database
     cursor = db.cursor()
 
-    #executes SQL command in Python
-    cursor.execute("SELECT * FROM states ORDER BY states.id")
+    # Execute the SQL query to retrieve states
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
+    # Fetch all the rows
     states = cursor.fetchall()
 
-    #print results of query 
+    # Display the results
     for state in states:
         print(state)
 
-#commit commands to execute
-db.commit()
+except MySQLdb.Error as e:
+    print(f"Error: {e}")
 
-#close all connections
-cursor.close()
-db.close()
-
-if __name__ == "__main__":
-    # Check if the correct number of arguments is provided
-    if len(sys.argv) != 4:
-        print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(sys.argv[0]))
-        sys.exit(1)
-
-    # Get MySQL credentials from command line arguments
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
-
-    # Call the select_states function with provided credentials
-    list_states(username, password, database)
+finally:
+    # Close the cursor and connection
+    if cursor:
+        cursor.close()
+    if db:
+        db.close()
