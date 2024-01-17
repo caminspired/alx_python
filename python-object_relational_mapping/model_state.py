7d7ab485a2420682eff346fee45ce6455e241d47
caminspired <charlotteaowusuansah@gmail.com>
2024-01-17 22:39:29 +0000

    6 orm

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,15 @@
+# this module contains the class State and an instance of class Base
+if __name__ == "__main__":
+    
+    # importing attributes from SQLAlchemy
+    from sqlalchemy import create_engine
+    from sqlalchemy import Column, Integer, String
+    from sqlalchemy.ext.declarative import declarative_base
+    
+    Base = declarative_base()
+    
+    # creating class States that inherits from Base
+    class State(Base):
+        __tablename__ = "states"
+        id = Column(Integer, primary_key=True, nullable=False)
+        name = Column(String(128), nullable=False)   
