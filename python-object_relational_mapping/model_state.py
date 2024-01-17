a5a528167ef7184c8e6923e6b662eb8019279fb6
caminspired <charlotteaowusuansah@gmail.com>
2024-01-17 23:01:53 +0000

    6 orm

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -2,24 +2,24 @@
 
 if __name__ == "__main__":
     
-    """importing attributes from SQLAlchemy"""
+"""importing attributes from SQLAlchemy"""
     from sqlalchemy import create_engine
     from sqlalchemy import Column, Integer, String
     from sqlalchemy.ext.declarative import declarative_base
 
-    """initiating instance Base"""
+"""initiating instance Base"""
     Base = declarative_base()
 
-    """creating an empty class States that inherits from Base"""
+"""creating an empty class States that inherits from Base"""
     class State(Base):
         
-        """
-        Creates an empty class State in a database.
+    """
+    Creates an empty class State in a database.
 
-        Attributes:
-        - id (int): The unique identifier for the state.
-        - name (str): The name of the state.
-        """
+    Attributes:
+    - id (int): The unique identifier for the state.
+    - name (str): The name of the state.
+    """
         
         __tablename__ = "states"
         id = Column(Integer, primary_key=True, nullable=False)
