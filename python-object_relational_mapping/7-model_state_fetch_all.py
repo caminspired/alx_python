66140800d3586ac92a5c75032bd34012aa7f25b4
caminspired <charlotteaowusuansah@gmail.com>
2024-01-18 18:43:53 +0000

    7 orm

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     
-    for state in session.query(State).order_by(State.id).all():
+    for states in session.query(State).order_by(State.id).all():
         print("{}: {}".format(states.id, states.name))
         
     session.close()
