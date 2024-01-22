e31d47466b041a68aacfec69532f97cafcf6115d
caminspired <charlotteaowusuansah@gmail.com>
2024-01-22 22:09:29 +0000

    4 flask

--- python-web_framework/4-number_route.py
+++ python-web_framework/4-number_route.py
@@ -26,7 +26,6 @@ def python_text(text='is cool'):
     return 'Python ' + text.replace('_', ' ')
 
 @app.route('/number/<n>', strict_slashes=False)
-@app.route('/number/', strict_slashes=False)
 def number(n):
     if type(n) == int:
         return n + ' is a number'
