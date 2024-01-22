5946fd93b935aa1ffc57b69ad6c661f5f58f3a5b
caminspired <charlotteaowusuansah@gmail.com>
2024-01-22 21:29:30 +0000

    2 flask

--- python-web_framework/2-c_route.py
+++ python-web_framework/2-c_route.py
@@ -19,7 +19,7 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     _ = ' '
-    return 'C ' + text
+    return 'C ' + text.replace('_', ' ')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
