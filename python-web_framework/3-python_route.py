ec6b5d10e9e172362d87528e107aee8c95e09261
caminspired <charlotteaowusuansah@gmail.com>
2024-01-22 21:37:06 +0000

    3 flask

---
+++ python-web_framework/3-python_route.py
@@ -0,0 +1,33 @@
+'''this module contains a script that creates web application using flask'''
+
+    
+'''importing flask'''
+from flask import Flask
+
+'''creates the variable application name'''
+app = Flask(__name__)
+
+'''route for retrieving hello HBNB'''
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    return 'C ' + text.replace('_', ' ')
+
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    text_value = text
+    if not text_value:
+        return 'Python is cool!'
+    else:
+        return 'Python ' + text.replace('_', ' ')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+    
