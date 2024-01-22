3da8b92ee680351d2fca58b2069a62e6aae3e9cc
caminspired <charlotteaowusuansah@gmail.com>
2024-01-22 21:43:58 +0000

    3 flask

--- python-web_framework/3-python_route.py
+++ python-web_framework/3-python_route.py
@@ -22,7 +22,7 @@ def c_text(text):
 
 @app.route('/python/<text>', strict_slashes=False)
 @app.route('/python/', strict_slashes=False)
-def python_text(text='is cool!'):
+def python_text(text='is cool'):
     return 'Python ' + text.replace('_', ' ')
 
 if __name__ == '__main__':
