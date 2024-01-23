d6302e3d897c813367c597d98d570d4c88cd5b3f
caminspired <charlotteaowusuansah@gmail.com>
2024-01-23 15:58:57 +0000

    6 flask

--- python-web_framework/6-number_odd_or_even.py
+++ python-web_framework/6-number_odd_or_even.py
@@ -35,6 +35,12 @@ def number(n):
 def number_template(n):
     return render_template('5-number.html', n=n)
 
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_template(n):
+    if n % 2 == 0:
+        return render_template('6-number_odd_or_even.html', n=n, x='even')
+    else:
+        return render_template('6-number_odd_or_even.html', n=n, x='odd')
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
