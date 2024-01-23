7bd4d3b40c21c7f774b6fbe18332ffaed172bd7b
caminspired <charlotteaowusuansah@gmail.com>
2024-01-23 14:25:23 +0000

    5 flask

--- python-web_framework/5-number_template.py
+++ python-web_framework/5-number_template.py
@@ -33,7 +33,7 @@ def number(n):
     
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n=int):
-    return render_template('5-number.html', n=n)
+    return render_template('5-number.html', n='<h1>n</h1>')
     if not n:
         return 404
     
