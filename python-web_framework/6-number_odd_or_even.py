1a1b16514b58c4c1d950aa2a0f5afaf1da1287b3
caminspired <charlotteaowusuansah@gmail.com>
2024-01-23 16:01:26 +0000

    6 flask

--- python-web_framework/6-number_odd_or_even.py
+++ python-web_framework/6-number_odd_or_even.py
@@ -7,34 +7,40 @@ from flask import Flask, render_template
 '''creates the variable application name'''
 app = Flask(__name__)
 
-'''route for retrieving hello HBNB'''
+'''retrieving route response'''
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return 'Hello HBNB!'
 
+'''retrieving route response'''
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return 'HBNB'
 
+'''retrieving route response'''
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     return 'C ' + text.replace('_', ' ')
 
+'''retrieving route response'''
 @app.route('/python/<text>', strict_slashes=False)
 @app.route('/python/', strict_slashes=False)
 def python_text(text='is cool'):
     return 'Python ' + text.replace('_', ' ')
 
+'''retrieving route response'''
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     return '{} is a number'.format(n)
     if not n:
         return 404
-    
+
+'''retrieving route response'''   
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     return render_template('5-number.html', n=n)
 
+'''retrieving route response'''
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_template(n):
     if n % 2 == 0:
