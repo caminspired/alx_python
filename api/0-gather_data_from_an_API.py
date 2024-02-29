f3ecc72072ade3464d7b57effa163383d1f3b8fa
caminspired <charlotteaowusuansah@gmail.com>
2024-02-29 12:15:22 +0000

    0 api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,38 +1,37 @@
 #Script to fetche employee todo list progress
 
+import requests
+import sys
+
+employee_id = sys.argv[1]
+
+def todo_list_progress(employee_id=int):
+    
+    #fetching employee details
+    
+    employee_details = requests.get("https://jsonplaceholder.typicode.com/users/{employee_id}")
+    employee_data = employee_details.json()
+    employee_name = employee_data['name']        
+    todos = len(employee_todos)
+    
+    #fetching employee todos
+    
+    employee_todos = requests.get("https://jsonplaceholder.typicode.com/users/{employee_id}/todos")
+    todos_details = employee_todos.json()
+    total_tasks = len(employee_details)
+    completed_tasks = 0
+    
+    for todo["completed"] in todos:
+        completed_tasks +1
+        
+    #printing employee todo list progress          
+    
+    print("Employee {} is done with tasks{}/{}".format(employee_name, completed_tasks, total_tasks))
+    
+todo_list_progress(employee_id)
+
+
 if __name__ == "__main__":
-
-
-    import requests
-    import sys
-    
-    employee_id = sys.argv[1]
-    
-    def todo_list_progress(employee_id=int):
-        
-        #fetching employee details
-        
-        employee_details = requests.get("https://jsonplaceholder.typicode.com/users/{employee_id}")
-        employee_data = employee_details.json()
-        employee_name = employee_data['name']        
-        todos = len(employee_todos)
-        
-        #fetching employee todos
-        
-        employee_todos = requests.get("https://jsonplaceholder.typicode.com/users/{employee_id}/todos")
-        todos_details = employee_todos.json()
-        total_tasks = len(employee_details)
-        completed_tasks = 0
-        
-        for todo["completed"] in todos:
-            completed_tasks +1
-            
-        #printing employee todo list progress          
-        
-        print("Employee {} is done with tasks{}/{}".format(employee_name, completed_tasks, total_tasks))
-        
-    todo_list_progress(employee_id)
-        
-        
-        
-    
+    if len(sys.argv) != 2:
+        print("Usage: python3 script.py <employee_id>")
+        sys.exit(1)
