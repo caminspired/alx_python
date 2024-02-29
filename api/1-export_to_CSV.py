3fb9567dcc2a1816992fc5b654c6b1d5a672c18b
caminspired <charlotteaowusuansah@gmail.com>
2024-02-29 13:36:13 +0000

    1 api

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,47 @@
+import requests
+import sys
+import csv
+
+employee_id = int(sys.argv[1]) 
+
+#fetching employee general details
+employee_details = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
+employee_data = employee_details.json()
+
+#fetching employee todo list details
+employee_todos = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos")
+todos_details = employee_todos.json()
+
+def todo_list_progress(employee_id):
+    
+    #fetching employee name
+    employee_name = employee_data["name"]
+    
+    #fetching employee total todo list length and completed tasks
+    total_tasks = len(todos_details)
+    completed_tasks = sum(1 for todo in todos_details if todo["completed"])
+    
+    #printing employee todo list progress          
+    print("Employee {} is done with tasks({}/{}):".format(employee_name, completed_tasks, total_tasks))
+
+    for todo in todos_details:
+        if todo["completed"]:
+            print(f"\t {todo['title']}")
+            
+    
+def write_to_csv(USER_ID, USERNAME, TASK_COMPLETED_STATUS, TASK_TITLE):
+     with open("USER_ID.csv", "w", newline="") as csvfile:
+         fieldnames = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
+         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+         
+         writer.writeheader()
+         writer.writerow({
+             "USER_ID": employee_id,
+             "USERNAME": employee_name,
+             "TASK_COMPLETED_STATUS":str(task_completed),
+             "TASK_TITLE": task_title
+         })
+    
+if __name__ == "__main__":   
+    todo_list_progress(employee_id)
+
